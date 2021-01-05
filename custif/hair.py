dc1ad26162cb0832def97246cf2f050365982bcd
Debora Bitencourt <deborabr21@gmail.com>
2021-01-05 23:18:20 +0000

    upload

---
+++ custif/hair.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#For every gray hair I find in my brush I'm going to scream!
+brush = ["gray", "gray", "brown", "blonde", "gray"]
+
+for hair in brush:
+    if hair == "gray":
+        print("NOOOOOOOOO!")
